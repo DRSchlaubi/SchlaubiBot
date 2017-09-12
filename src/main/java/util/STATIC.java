c871f36f365554c1f828eb5c706d3087f9305f5e
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-09-12 19:51:42 +0200

    Minor bug fixes - Fixed some bugs

--- src/main/java/util/STATIC.java
+++ src/main/java/util/STATIC.java
@@ -5,7 +5,7 @@ public class STATIC {
 
     public static final String game = "SchlaubiBot v" + STATIC.VERSION + " | " + STATIC.prefix + "help | Made by Schlaubi";
     public static final String prefix = "::";
-    public static final String VERSION = "1.6.0";
+    public static final String VERSION = "1.6.2";
     public static final String[] PERMS = {"Administrator", "Admin", "Friend", "Moderator", "Supporter", "Bot Admin", "Bot Owner"};
     public static final String[] OWNERS = {"264048760580079616", "240797338430341120", "240797338430341120", "257187269943754752"};
     public static final String BITLYUSERNAME = "o_5uh653n9rj";
