c1e6b63b6ba0b1179f75c047a99f13d8a2c4080e
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-09-15 17:02:46 +0200

    Removed SECRETS.java

--- src/main/java/net/Schlaubi/SchlaubiBot/util/SECRETS.java
+++ src/main/java/net/Schlaubi/SchlaubiBot/util/SECRETS.java
@@ -1,6 +1,6 @@
 package net.Schlaubi.SchlaubiBot.util;
 
 public class SECRETS {
-    public static String token = "MzE4MDExMjk5MjY5MzEyNTEy.DJb6Mg.AmI8kTgFgrk7xU3Woml5FpX13jc";
-    public static String bitlytoken = "R_4e2b6b828df1461ba2dc097b444cff69";
+    public static String token = "MzE4MDExMjk5MjY5MzEyNTEy.DJ185A.IclZTIa-VZcm5o5Q52RdcZkj3A4";
+    public static String bitlytoken = "R_98f8b53bc0b747f08d522e92cbbbf6ee";
 }
